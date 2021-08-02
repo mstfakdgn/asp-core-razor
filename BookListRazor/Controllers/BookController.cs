eb1f257d88dd7447fbc3292ac3ca4f6a607779d1
mdskln2565 <mustafa.akdogan@meramedas.com.tr>
2021-08-02 14:35:54 +0300

    first commit

---
+++ BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Book.ToList() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookDb =  await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Book.Remove(bookDb);
+            await _db.SaveChangesAsync();
+
+            return Json(new { success = true, message = "Deleted" });
+        }
+    }
+}
